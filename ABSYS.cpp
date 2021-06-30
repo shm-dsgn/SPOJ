bb7dab129a03907a06c0cf380c91cb1a315fa779
Soham Dutta <77527904+shm-dsgn@users.noreply.github.com>
2021-06-30 17:14:27 +0530

    Add files via upload

---
+++ ABSYS.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int convert(string s)
+{
+    int num;
+    num = 0;
+    int i;
+
+    for(i = 0; i < s.length(); i++) {
+        if(!isdigit(s[i])) {
+            return -1;
+        }
+
+        num = (num * 10) + ((int)s[i] - 48);
+    }
+    return num;
+}
+int main()
+{
+   string s1,s2,s3,s4,s5;
+   int v1;
+   int v2;
+   int v3;
+   int t;
+
+   cin>>t;
+   while(t--) 
+   {
+		cin>>s1>>s2>>s3>>s4>>s5;
+
+   		v1 = convert(s1);
+   		v2 = convert(s3);
+   		v3 = convert(s5);
+
+        if(v1<0) v1=v3-v2;
+        if(v2<0) v2=v3-v1;
+        if(v3<0) v3=v1+v2;
+         cout<<v1<<" + "<<v2<<" = "<<v3<<endl;
+
+
+   }
+
+   return 0;
+
+
+
+}
